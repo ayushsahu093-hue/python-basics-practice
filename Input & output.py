6a21168662b9ed8e0009e8c767912780a85c5396
Aayush Sahu <ayushsahu093@gmail.com>
2026-02-18 00:55:25 +0530

    Add examples for output, input, and calculator

---
+++ Input & output.py
@@ -0,0 +1,53 @@
+
+# ------------------------------------------
+# Example 1: Simple Output
+# ------------------------------------------
+
+print("Example 1: Simple Output")
+print("Hello, World!")
+print("Welcome to Python!")
+print()
+
+
+# ------------------------------------------
+# Example 2: Simple Input (Name Greeting)
+# ------------------------------------------
+
+print("Example 2: User Input (Name)")
+name = input("Enter your name: ")
+print("Hello", name)
+print()
+
+
+# ------------------------------------------
+# Example 3: Simple Calculator (Addition)
+# ------------------------------------------
+
+print("Example 3: Simple Addition Calculator")
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+sum_result = num1 + num2
+
+print("The sum is:", sum_result)
+
+# ------------------------------------------
+# Example 3: Simple Calculator (multiplication)
+# ------------------------------------------
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+multi_result = num1*num2
+
+print("The multiplication is:", multi_result)
+
+
+
+
+
+
+
+
+
